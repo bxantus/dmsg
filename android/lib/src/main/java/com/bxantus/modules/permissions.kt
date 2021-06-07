5bb9e81bfae0e0c3ee5d9b1abd2a22c12ce10488
bxantus <drx.trash@gmail.com>
2021-06-07 11:52:21 +0200

    Draft of permissions module - permission managaer converts the request call to a suspend (async) function - webActivity can be used as a building block, or base activity for apps with Messaging enabled

--- android/lib/src/main/java/com/bxantus/modules/permissions.kt
+++ android/lib/src/main/java/com/bxantus/modules/permissions.kt
@@ -1,7 +1,42 @@
 package com.bxantus.modules
 
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import com.bxantus.messaging.Module
+import kotlinx.coroutines.*
 
 class Permissions : Module() {
     
+}
+
+class PermissionManager(private val webActivity: WebActivity) {
+    data class CheckResult(val granted:Boolean, val shouldShowRationale:Boolean)
+
+    fun check(permission:String):CheckResult {
+        val granted = ContextCompat.checkSelfPermission(webActivity, permission)
+        var shouldShowRationale = false
+        if (granted == PackageManager.PERMISSION_DENIED) {
+            shouldShowRationale = webActivity.shouldShowRequestPermissionRationale(permission)
+        }
+        return CheckResult(granted == PackageManager.PERMISSION_GRANTED, shouldShowRationale)
+    }
+
+    private val requests = mutableListOf<Int>()
+    suspend fun request(permission:String):Boolean {
+        val requestCode = requests.getOrElse(requests.lastIndex) { 0 } + 1
+        requests.add(requestCode)
+        val response = CompletableDeferred<Boolean>()
+        webActivity.addPermissionHandler{ forRequest, permissions, results->
+            if (forRequest == requestCode) {
+                // resolve outer suspended function with the results
+                if (permissions.isNotEmpty() && results.isNotEmpty()) { // not cancelled
+                    response.complete(results[0] == PackageManager.PERMISSION_GRANTED)
+                } else response.complete(false) // treat cancelled as permission denied
+            }
+            // todo: should use a single permission handler for all requests
+            //       requests can store the deferred response
+        }
+        webActivity.requestPermissions(arrayOf(permission), requestCode)
+        return response.await()
+    }
 }
---
+++ android/lib/src/main/java/com/bxantus/modules/webactivity.kt
@@ -0,0 +1,26 @@
+package com.bxantus.modules
+
+import android.app.Activity
+
+typealias PermissionHandler = (requestCode: Int,
+                               permissions: Array<out String>,
+                               grantResults: IntArray) -> Unit
+
+class WebActivity : Activity() {
+    private val permissionHandlers = mutableListOf<PermissionHandler>()
+    fun addPermissionHandler(handler:PermissionHandler) {
+        permissionHandlers.add(handler)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        // will dispatch request results to all handlers
+        // if handlers maintain their request codes correctly, multiple handlers won't receive the same results
+        for (handler in permissionHandlers) {
+            handler(requestCode, permissions, grantResults);
+        }
+    }
+}
