d140887ff46d08fef04c383e3ff764974504a1a6
bxantus <drx.trash@gmail.com>
2021-06-14 12:55:30 +0200

    Update Android LocationProvider interface on ts side

--- ts/src/lib/location/android/location.ts
+++ ts/src/lib/location/android/location.ts
@@ -1,3 +1,5 @@
+import { RemoteObj } from '../../../messaging/messagingConnection.ts'
+
 export interface LocationModule {
     readonly locationProvider:LocationProvider
 }
@@ -14,4 +16,10 @@ export interface Location {
 
 export interface LocationProvider {
     getLastLocation():Promise<Location>
+    createLocationRequest(interval:number):Promise<RemoteObj>
+    /// checks whether settings are enabled for this kind of location request
+    /// if not it will try to show system popup which will enable location
+    checkSettings(forRequest:RemoteObj):boolean
+    /// starts location updates for the request created with createLocationRequest
+    requestUpdates(req:RemoteObj, listener:(loc:Location)=>void):Promise<void>
 }
