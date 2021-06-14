d06277cd1bffed272491bbf7fef1d46ea44c002e
bxantus <drx.trash@gmail.com>
2021-06-14 15:48:28 +0200

    Created generic interface for LocationProvider based on android impl - added implementation for it in the android code - updated AndroidLocationProvider typings

--- ts/src/lib/location/android/location.ts
+++ ts/src/lib/location/android/location.ts
@@ -1,16 +1,55 @@
 import { RemoteObj } from '../../../messaging/messagingConnection.ts'
-import { Location } from "../locationService.ts"
+import { Location, ILocationProvider, LocationUpdateListener, LocationUpdateRequest } from "../locationService.ts"
 
 export interface LocationModule {
-    readonly locationProvider:LocationProvider
+    readonly locationProvider:AndroidLocationProvider
 }
 
-export interface LocationProvider {
+export interface AndroidLocationProvider {
     getLastLocation():Promise<Location>
     createLocationRequest(interval:number):Promise<RemoteObj>
     /// checks whether settings are enabled for this kind of location request
     /// if not it will try to show system popup which will enable location
-    checkSettings(forRequest:RemoteObj):boolean
+    checkSettings(forRequest:RemoteObj):Promise<boolean>
     /// starts location updates for the request created with createLocationRequest
-    requestUpdates(req:RemoteObj, listener:(loc:Location)=>void):Promise<void>
+    /// returns the android locationCb which can be used to stop requests
+    requestUpdates(req:RemoteObj, listener:LocationUpdateListener):Promise<RemoteObj>
+    stopUpdates(handle:RemoteObj):Promise<void>
+}
+
+export class LocationProvider implements ILocationProvider {
+    private androidProvider:AndroidLocationProvider
+    private lastRequest:{ obj:RemoteObj, interval:number }|undefined 
+    
+    constructor(mod:LocationModule) {
+        this.androidProvider = mod.locationProvider
+    }
+    
+    getLastLocation() {
+        return this.androidProvider.getLastLocation()
+    }
+    
+    async checkSettings(forRequest:LocationUpdateRequest) {
+        return this.androidProvider.checkSettings(await this.getAndroidRequest(forRequest))
+    }
+
+    
+    async requestUpdates(req:LocationUpdateRequest, listener:LocationUpdateListener) {
+        return this.androidProvider.requestUpdates(await this.getAndroidRequest(req), listener)
+    }
+
+    stopUpdates(handle:any) {
+        return this.androidProvider.stopUpdates(handle)
+    }
+
+    private async getAndroidRequest(forRequest:LocationUpdateRequest) {
+        let request:RemoteObj
+        if (this.lastRequest?.interval == forRequest.interval) 
+            request = this.lastRequest.obj
+        else {
+            request = await this.androidProvider.createLocationRequest(forRequest.interval)
+            this.lastRequest = { obj: request, interval:forRequest.interval }
+        }
+        return request
+    }
 }
--- ts/src/lib/location/locationService.ts
+++ ts/src/lib/location/locationService.ts
@@ -9,9 +9,29 @@ export interface Location {
     speed?:number
 }
 
+// todo: this interface is deprecated, rewrite usages to use ILocationProvider (see below)
 export interface LocationService {
     currentPosition?:Location // this can be the last know position too
     heading?:number  // if available it corresponds to user heading: 0 north, 90 east etc.
     live:boolean     // when false currentPosition is the last known position
     // todo: add other methods based on android an iOS (maybe web location) interfaces
+}
+
+export interface LocationUpdateRequest {
+    // the minimum interval to receive location updates
+    interval:number
+}
+
+export type LocationUpdateListener = (loc:Location)=>void
+
+export interface ILocationProvider {
+    getLastLocation():Promise<Location>
+    /// checks whether settings are enabled for this kind of location request
+    /// if not it will try to show system popup which will enable location
+    // NOTE: should check if iOS has something similar
+    checkSettings(forRequest:LocationUpdateRequest):Promise<boolean>
+    /// starts location updates for the request, returns a handle for the request
+    requestUpdates(req:LocationUpdateRequest, listener:LocationUpdateListener):Promise<any>
+    /// stop location updates for the given handle
+    stopUpdates(handle:any):Promise<void>
 }
