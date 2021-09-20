61c9295acaabd92462a82f889464d1ae742f6854
bxantus <drx.trash@gmail.com>
2021-09-20 13:09:19 +0200

    Refactored MainActivity: moved webview and the common setup code to WebActivity - introduced a new abstract method: initialize, implement it to add your modules and return with the starting page's relative url

--- android/lib/src/main/java/com/bxantus/modules/webactivity.kt
+++ android/lib/src/main/java/com/bxantus/modules/webactivity.kt
@@ -2,7 +2,13 @@ package com.bxantus.modules
 
 import android.app.Activity
 import android.content.Intent
-import android.util.Log
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.webkit.WebViewAssetLoader
+import com.bxantus.messaging.android.MessagingInterface
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.Deferred
 
@@ -13,7 +19,7 @@ typealias PermissionHandler = (requestCode: Int,
 class ActivityRequest(public val requestCode:Int, public val response:Deferred<Int>) {
 }
 
-open class WebActivity : Activity() {
+abstract class WebActivity : Activity() {
     private val permissionHandlers = mutableListOf<PermissionHandler>()
     private val activityRequests = mutableListOf<ActivityRequest>()
     private val pendingActivityRequests = mutableMapOf<Int, CompletableDeferred<Int>>()
@@ -48,5 +54,46 @@ open class WebActivity : Activity() {
         val response = pendingActivityRequests.remove(requestCode) ?: return
         activityRequests.removeAt(activityRequests.indexOfFirst { it.requestCode == requestCode })
         response.complete(resultCode) // NOTE: maybe data may be added too to response
+    }
+
+    @ExperimentalStdlibApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WebView.setWebContentsDebuggingEnabled(true)
+
+        var webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+
+        val messaging = MessagingInterface(webView)
+        webView.addJavascriptInterface(messaging, "androidMessaging")
+        setupWebView(webView)
+
+        val startPage = initialize(messaging)
+        webView.loadUrl("https://appassets.androidplatform.net/assets/$startPage")
+
+        // set this webView as the main content in the activity
+        setContentView(webView)
+    }
+
+    /**
+     * Export any needed modules by your app via messaging
+     * @return the starting url of your app to be loaded inside the webView
+     *         the Url should be relative to the assets folder, webActivity will prepend the hostName
+     *         (ex. 'www/index.html')
+     */
+    @ExperimentalStdlibApi
+    abstract fun initialize(messaging:MessagingInterface):String
+
+    private fun setupWebView(webView:WebView) {
+        val assetLoader = WebViewAssetLoader.Builder() // by default uses: https://appassets.androidplatform.net
+            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this)).build()
+        webView.webViewClient = object: WebViewClient() {
+            override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
+                return if (request != null)
+                    assetLoader.shouldInterceptRequest(request.url)
+                else return super.shouldInterceptRequest(view, request as WebResourceRequest)
+            }
+        }
     }
 }
