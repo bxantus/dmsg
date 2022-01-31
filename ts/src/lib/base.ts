770dd950e0350102f964afe332ead465c1339241
bxantus <drx.trash@gmail.com>
2022-01-31 13:45:04 +0100

    Maybeobservable + dispose utility

--- ts/src/lib/base.ts
+++ ts/src/lib/base.ts
@@ -5,3 +5,12 @@ export function make<T>(v:T) { return v }
 export interface Disposable {
     dispose():void
 }
+
+export function dispose(...objects:(Disposable|Disposable[])[]) {
+    for (const obj of objects) {
+        if (obj instanceof Array) {
+            for (const o of obj) o.dispose()
+            obj.splice(0)
+        } else obj.dispose()
+    }
+}
